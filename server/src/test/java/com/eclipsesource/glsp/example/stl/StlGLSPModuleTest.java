32fe6ece16b652b738d303ff9a0ecec4edfa483a
Josef Sochovsky <josef.sochovsky@gmail.com>
2019-05-28 16:08:54 +0200

    added testcase for stlglspmodule fixes #6

--- server/src/test/java/com/eclipsesource/glsp/example/stl/StlGLSPModuleTest.java
+++ server/src/test/java/com/eclipsesource/glsp/example/stl/StlGLSPModuleTest.java
@@ -1,5 +1,25 @@
-import static org.junit.Assert.*;
+package com.eclipsesource.glsp.example.stl;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StlGLSPModuleTest {
 
+    private StlGLSPModule stlGLSPModule;
+
+    @Before
+    public void init() {
+        stlGLSPModule = new StlGLSPModule();
+    }
+
+    @Test
+    public void bindOperationHandlersNotNull() {
+        Assert.assertNotNull(stlGLSPModule.bindOperationHandlers());
+    }
+
+    @Test
+    public void bindDiagramConfigurationsNotNull() {
+        Assert.assertNotNull(stlGLSPModule.bindDiagramConfigurations());
+    }
 }
